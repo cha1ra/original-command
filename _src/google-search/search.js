e70a54ce6d293cb8fc06e7f49c719c4816149a34
cha1ra <cha1ra.garnet@gmail.com>
2019-06-19 18:12:27 +0900

    設定ファイルの読み込みを変更

--- _src/google-search/search.js
+++ _src/google-search/search.js
@@ -15,13 +15,13 @@ if(process.argv.length > 2){
       {waitUntil: "domcontentloaded"})
 
   const titles = await page.$$eval('.LC20lb', list => {
-    return list.map(data => data.textContent);
+    return list.map(data => data.textContent)
   })
   const links = await page.$$eval('.TbwUpd', list => {
-    return list.map(data => data.textContent);
+    return list.map(data => data.textContent)
   })
   const descs = await page.$$eval('.st', list => {
-    return list.map(data => data.textContent);
+    return list.map(data => data.textContent)
   })
   for(let i=0; i<titles.length; i++){
     console.log(`\u001b[32m ${titles[i]}\n\u001b[34m ${links[i]}\n\u001b[37m ${descs[i]}\n`)
